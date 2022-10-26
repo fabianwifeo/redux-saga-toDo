edbb9cd69e79bee7808b8d17957a41f69ba9870e
fabianwifeo <fabian.wifeo@gmail.com>
2022-10-26 09:52:09 +0200

    Adding form logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,15 +1,51 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function Form(){
+
+    const [newTask, setNewTask] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const clearInput = () => {
+        setNewTask('');
+    }
+
+    const addNewTask = () => {
+
+        if(newTask === '' || newTask.length > 30){
+            setErrorMessage('Please add a valid task (maximum 30 characters)');
+            return;
+        }
+        setErrorMessage('');
+        console.log(newTask);
+        clearInput();
+    }
 
     return (
         <div>
             <form>
-                <input type="text" className="addTask-input" required/>
-                <button className="addTask-button"><FontAwesomeIcon icon={faPlus}/></button>
+                <input 
+                type="text" 
+                className="addTask-input" 
+                required
+                value={newTask}
+                onInput={(e) => {
+                    setNewTask(e.target.value);
+                    console.log(newTask);
+                }}
+                />
+                <button 
+                className="addTask-button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    addNewTask()
+                }}
+                >
+                    <FontAwesomeIcon icon={faPlus}/>
+                </button>
+            <p className="error-message">{errorMessage}</p>
             </form>
-            <p className="error-message"></p>
         </div>
     )
 }
